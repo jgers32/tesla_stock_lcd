3941da1f0e072b820b946808c11d7b0c18c35141
julia <71852943+jgers32@users.noreply.github.com>
2020-12-18 00:15:44 -0500

    Create stock.py

---
+++ stock.py
@@ -0,0 +1,2 @@
+#this is a simple file that prints the $TSLA stats right out onto the shell. this gives good insight that everything is working correctly
+#all you have to do is take this info and make it display on the lcd display!
