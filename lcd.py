cb759fdbf7ba628d8c82898ed5c655ccdbd94018
julia <71852943+jgers32@users.noreply.github.com>
2020-12-18 00:44:13 -0500

    Create lcd.py

---
+++ lcd.py
@@ -0,0 +1,5 @@
+#https://finnhub.io/docs/api
+#the above url is for api documentation (finnhub) 
+#https://github.com/Finnhub-Stock-API/finnhub-python
+#their github was also very helpful in setting up the api
+
