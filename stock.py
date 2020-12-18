90dbd5499c91b80a7ae4760a808ce30d385d66e1
julia <71852943+jgers32@users.noreply.github.com>
2020-12-18 14:50:51 -0500

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,2 +1,3 @@
-#this is a simple file that prints the $TSLA stats right out onto the shell. this gives good insight that everything is working correctly
-#all you have to do is take this info and make it display on the lcd display!
+#https://github.com/twelvedata/twelvedata-python 
+
+#using twelve data api to get real time stock data- library looks extremely easy to use!
