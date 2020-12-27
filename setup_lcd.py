91e431b9bcc26125014f971fdccce11cccf9be51
julia <71852943+jgers32@users.noreply.github.com>
2020-12-27 17:57:16 -0500

    Delete setup_lcd.py

--- setup_lcd.py
+++
@@ -1,3 +0,0 @@
-#this code sets up the lcd to where you can just go ad change the code to change the output message on the display
-#granted- it is assumed that you have the same pins and connections that i do on mine 
-
